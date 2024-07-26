faf297143f17c4d064569f3a9ed8bd4410e5830b
doinki <doinki94@gmail.com>
2024-07-26 19:04:42 +0900

    ✨

--- apps/tera/vite.config.ts
+++ apps/tera/vite.config.ts
@@ -3,26 +3,30 @@ import { vitePlugin as remix } from '@remix-run/dev';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  build: {
-    target: ['chrome89', 'edge89', 'safari15', 'firefox89'],
-  },
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
-    devServer({
-      entry: 'server/server.development.js',
-      exclude: [...defaultOptions.exclude, /^\/app\/.*/, /.*\.png$/],
-      injectClientScript: false,
-    }),
-    tsconfigPaths(),
-  ],
-  server: {
-    host: '0.0.0.0',
-  },
+export default defineConfig(({ isSsrBuild }) => {
+  return {
+    build: {
+      target: isSsrBuild
+        ? 'node22'
+        : ['chrome89', 'edge89', 'safari15', 'firefox89'],
+    },
+    plugins: [
+      remix({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+        },
+      }),
+      devServer({
+        entry: 'server/server.development.js',
+        exclude: [...defaultOptions.exclude, /^\/app\/.*/, /.*\.png$/],
+        injectClientScript: false,
+      }),
+      tsconfigPaths(),
+    ],
+    server: {
+      host: '0.0.0.0',
+    },
+  };
 });
