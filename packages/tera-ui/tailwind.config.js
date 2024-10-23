4573d953bef52496463169f8435f8508a0608e15
doinki <doinki94@gmail.com>
2024-10-24 01:29:04 +0900

    ✨

--- packages/tera-ui/tailwind.config.js
+++ packages/tera-ui/tailwind.config.js
@@ -4,6 +4,9 @@
 export default {
   content: ['./src/**/*.{ts,tsx}', './story/**/*.{ts,tsx}'],
   darkMode: 'class',
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     extend: {
       borderColor: {
